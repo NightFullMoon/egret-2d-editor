a53532d98eb1b5034d5441a4777f8943c54ee809
Hong <2406799112@qq.com>
2020-08-14 09:09:54 +0800

    树结构的组件

---
+++ src/panels/scene/scene-node.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    {{ element.url }}
+    <sceneNode
+      v-for="(element, index) in element.children"
+      :element="element"
+      :key="index"
+    ></sceneNode>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    element: {
+      type: Object,
+      default: () => {
+        return {};
+      }
+    }
+  }
+  //   components: {
+  //     sceneNode
+  //   }
+};
+</script>
+
+<style></style>
--- src/panels/scene/scene-tree.vue
+++ src/panels/scene/scene-tree.vue
@@ -1,7 +1,17 @@
+<template>
+  <div>
+    <sceneNode :element="sceneInfo"></sceneNode>
+  </div>
+</template>
 <script>
 // 场景的面板
-
+// import { treeMap } from "@/editor-runtime/utils/Tree";
+import sceneNode from "./scene-node";
 import SceneManger from "@/scene-manager";
+import Vue from "vue";
+
+Vue.component("sceneNode", sceneNode);
+
 // import Vue from "vue";
 
 const instance = SceneManger();
@@ -9,16 +19,32 @@ const instance = SceneManger();
 export default {
   computed: {
     sceneInfo() {
-      return instance.getScene();
+      return instance.getScene().sceneInfo;
     }
-  },
-  watch: {
+  }
+  /*   watch: {
     sceneInfo() {
       console.log(this.sceneInfo);
     }
+  } */
+
+  /*   methods: {
+    renderNode(element) {
+      // const h = this.$createElement;
+      return (
+        <div>
+          <span>{element.url}</span>
+          <ul>{element.children}</ul>
+        </div>
+      );
+    }
   },
-  render(h) {
-    return h("div", "123123");
-  }
+
+  render() {
+    return treeMap(this.sceneInfo, element => {
+      console.log(element);
+      return this.renderNode(element);
+    });
+  } */
 };
 </script>
